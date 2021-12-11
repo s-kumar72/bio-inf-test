40071c0781d832fc36571db51ac2b2410390e136
Sadhana Kumar <67485942+s-kumar72@users.noreply.github.com>
2021-12-10 21:55:23 -0600

    created script.py 12-10-2021

---
+++ script.py
@@ -0,0 +1,11 @@
+import requests
+import argparse
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+
+with open('a_b_hydrolase_100.txt') as f:
+    lines = f.readlines() + " "
+
