730a9b11bdffd920967daf048134cd3590c923ee
Sadhana Kumar <67485942+s-kumar72@users.noreply.github.com>
2021-12-12 10:01:12 -0600

    prompt os to make dir 'data'

--- web_scraper.py
+++ web_scraper.py
@@ -1,13 +1,15 @@
 #! /usr/bin/python
 
 import requests
+import os
 
 def fetch_cif_file(pdb_id):
+        os.mkdir('~/github/bio-inf-test/data')
         # URL to download PDB/mmCIF files from RCSB
         base_url = 'https://files.rcsb.org/download/PDBID.cif'
 
         for id in pdb_id:
-                r = requests.get(base_url.replace('PDBID',id,-1))
+                r = requests.get(base_url.replace('PDBID', id, -1))
                 file_name = "data/" + id + ".cif"
                 with open(file_name, 'wb') as fd:
                         fd.write(r.text)
