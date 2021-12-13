8f56f916da2defd23d38bace26635de57fbff6e9
Sadhana Kumar <67485942+s-kumar72@users.noreply.github.com>
2021-12-13 08:48:55 -0600

    make cif_data folder

--- web_scraper.py
+++ web_scraper.py
@@ -4,12 +4,15 @@ import requests
 import os
 
 def fetch_cif_file(pdb_id):
-        os.mkdir('~/github/bio-inf-test/data')
+        path = 'cif_data'
+        if not os.path.exists(path):
+            os.mkdir('cif_data')
         # URL to download PDB/mmCIF files from RCSB
         base_url = 'https://files.rcsb.org/download/PDBID.cif'
 
         for id in pdb_id:
                 r = requests.get(base_url.replace('PDBID', id, -1))
-                file_name = "data/" + id + ".cif"
-                with open(file_name, 'wb') as fd:
+                file_name = id + '.cif'
+                file_loc = os.path.join('cif_data/', file_name)
+                with open(file_loc, 'wb') as fd:
                         fd.write(r.text)
